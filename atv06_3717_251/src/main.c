48fde2d47df4fa8dab80390af837e9bbba163637
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2025-06-02 18:00:11 -0300

    feat: adicionando o uso dos botões

--- atv06_3717_251/src/main.c
+++ atv06_3717_251/src/main.c
@@ -4,6 +4,9 @@
 #include <stdint.h>
 
 #include "lcd.h"
+#include "btn.h"
+
+
 
 // --- Função para mostrar bits como string com espaços ---
 void bits_to_string(uint8_t val, char* str) {
@@ -18,9 +21,31 @@ void bits_to_string(uint8_t val, char* str) {
 
 int main() {
     lcd_init();
+    btn_init();
+    
     lcd_print("Hello World");
+    lcd_goto(1, 0);
+    lcd_print("Press buttons:");
     
     while(1) {
-        // Loop infinito
+        if (btn_read(BTN_S1)) {
+            lcd_goto(1, 14);
+            lcd_print("S1");
+            _delay_ms(200);
+        }
+        else if (btn_read(BTN_S2)) {
+            lcd_goto(1, 14);
+            lcd_print("S2");
+            _delay_ms(200);
+        }
+        else if (btn_read(BTN_S3)) {
+            lcd_goto(1, 14);
+            lcd_print("S3");
+            _delay_ms(200);
+        }
+        else {
+            lcd_goto(1, 14);
+            lcd_print("  ");
+        }
     }
 }
