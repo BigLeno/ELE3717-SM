29b6c4268ee2806fa87a4d0558f5e8078e3a7fa8
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2025-06-02 18:04:12 -0300

    feat: iniciando a mde

--- atv06_3717_251/src/main.c
+++ atv06_3717_251/src/main.c
@@ -6,7 +6,11 @@
 #include "lcd.h"
 #include "btn.h"
 
-
+// Estados da máquina de estados
+typedef enum {
+    STATE_INITIAL,
+    STATE_BUTTONS
+} state_t;
 
 // --- Função para mostrar bits como string com espaços ---
 void bits_to_string(uint8_t val, char* str) {
@@ -23,29 +27,53 @@ int main() {
     lcd_init();
     btn_init();
     
-    lcd_print("Hello World");
-    lcd_goto(1, 0);
-    lcd_print("Press buttons:");
+    state_t current_state = STATE_INITIAL;
     
     while(1) {
-        if (btn_read(BTN_S1)) {
-            lcd_goto(1, 14);
-            lcd_print("S1");
-            _delay_ms(200);
-        }
-        else if (btn_read(BTN_S2)) {
-            lcd_goto(1, 14);
-            lcd_print("S2");
-            _delay_ms(200);
-        }
-        else if (btn_read(BTN_S3)) {
-            lcd_goto(1, 14);
-            lcd_print("S3");
-            _delay_ms(200);
-        }
-        else {
-            lcd_goto(1, 14);
-            lcd_print("  ");
+        switch(current_state) {
+            case STATE_INITIAL:
+                lcd_clear();
+                lcd_goto(0, 0);
+                lcd_print("ELE-3717");
+                lcd_goto(1, 0);
+                lcd_print("FILTRO FIR");
+                
+                // Aguarda qualquer botão para mudar de estado
+                while(!btn_read(BTN_S1) && !btn_read(BTN_S2) && !btn_read(BTN_S3)) {
+                    _delay_ms(50);
+                }
+                current_state = STATE_BUTTONS;
+                _delay_ms(300); // Debounce
+                break;
+                
+            case STATE_BUTTONS:
+                lcd_clear();
+                lcd_goto(0, 0);
+                lcd_print("Press buttons:");
+                
+                while(current_state == STATE_BUTTONS) {
+                    if (btn_read(BTN_S1)) {
+                        lcd_goto(1, 0);
+                        lcd_print("S1 pressed     ");
+                        _delay_ms(200);
+                    }
+                    else if (btn_read(BTN_S2)) {
+                        lcd_goto(1, 0);
+                        lcd_print("S2 pressed     ");
+                        _delay_ms(200);
+                    }
+                    else if (btn_read(BTN_S3)) {
+                        lcd_goto(1, 0);
+                        lcd_print("S3 pressed     ");
+                        _delay_ms(200);
+                    }
+                    else {
+                        lcd_goto(1, 0);
+                        lcd_print("               ");
+                    }
+                    _delay_ms(50);
+                }
+                break;
         }
     }
 }
